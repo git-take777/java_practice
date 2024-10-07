7618ccb11ea4d91e0b58cc8bcdf6f5a2845bd420
git-take777 <git.coding.life@gmail.com>
2024-10-07 19:25:43 +0900

    Java 配列の書き方

---
+++ Array/arr.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class arr {
+  public static void main(String[] args) {
+    // データ型と変数名(配列名)を定義
+    String[] arr;
+    arr = new String[3];
+
+    arr[0] = "A";
+    arr[1] = "B";
+    arr[2] = "C";
+
+    System.out.println(arr[0]);
+    System.out.println(arr[1]);
+    System.out.println(arr[2]);
+
+  }
+
+  // String[] arr2 = {"20", "40", "50"};
+  //   System.out.println(arr2[0]);
+  //   System.out.println(arr2[1]);
+  //   System.out.println(arr2[2]);
+}
