89a3c02a55e85992b84dc3b1a683366e62b96b54
git-take777 <git.coding.life@gmail.com>
2024-10-07 19:24:00 +0900

    Java 基礎 javaコードの書き方

---
+++ Greeting/Greeting.java
@@ -0,0 +1,8 @@
+package Greeting;
+class Greeting {
+  public static void main(String[] args) {
+    System.out.println("Good Moring!");
+    System.out.println("Good afternonn!");
+    System.out.println("Good evening!");
+  }
+}
